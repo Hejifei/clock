bf5017a0c8d3d4688376dc2459d23e2f2d3d756d
hejifei <jifei.he@goodwe.com>
2023-11-07 20:50:20 +0800

    初步完成

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -1,16 +1,19 @@
 // index.ts
 import Toast from '@vant/weapp/toast/toast';
+import moment from 'moment'
 // 获取应用实例
-const app = getApp<IAppOption>()
+// const app = getApp<IAppOption>()
 
 Page({
     data: {
-        interval: 0.25,
+        interval: 0,
         numberOfReminders: 3,
         leftNumberOfReminders: 3,
-        time: 15 * 1000,
+        time: 5 * 1000,
         timeData: {},
         timer: null,
+        recordList: [],
+        songPlaySeconds: 3,
     },
     // 事件处理函数
     bindViewTap() {
@@ -19,42 +22,52 @@ Page({
         })
     },
     onLoad() {
+        console.log('onShow')
         const that = this;
-        // wx.getStorage({
-        //     key: "timtConfig",
-        //     success(res) {
-        //         let resObj: {
-        //             interval?: number
-        //             numberOfReminders?: number
-        //         } = {}
-        //         try {
-        //             resObj = JSON.parse(res.data)
-        //         } catch (err) {
-        //             console.log(err)
-        //         }
-        //         that.setData({
-        //             interval: resObj?.interval || 10,
-        //             numberOfReminders: resObj?.numberOfReminders || 10,
-        //             leftNumberOfReminders: resObj?.numberOfReminders || 10,
-        //             time: (resObj?.interval || 10) * 60 * 1000,
-        //         })
-        //     },
-        //     fail(res) {
-        //         that.setData({
-        //             interval: 10,
-        //             numberOfReminders: 3,
-        //             leftNumberOfReminders: 3,
-        //             time: 10 * 60 * 1000,
-        //         })
-        //     }
-        // })
+        wx.getStorage({
+            key: "timtConfig",
+            success(res) {
+                let resObj: {
+                    interval?: number
+                    numberOfReminders?: number
+                    songPlaySeconds?: number
+                } = {}
+                try {
+                    resObj = JSON.parse(res.data)
+                } catch (err) {
+                    console.log(err)
+                }
+                that.setData({
+                    interval: resObj?.interval || 10,
+                    numberOfReminders: resObj?.numberOfReminders || 10,
+                    leftNumberOfReminders: resObj?.numberOfReminders || 10,
+                    time: (resObj?.interval || 10) * 60 * 1000,
+                    songPlaySeconds: resObj?.songPlaySeconds || 10,
+                })
+            },
+            fail(res) {
+                that.setData({
+                    interval: 10,
+                    numberOfReminders: 3,
+                    leftNumberOfReminders: 3,
+                    time: 10 * 60 * 1000,
+                    songPlaySeconds: 10,
+                })
+            }
+        })
     },
-    onChange(e) {
+    onChange(e: any) {
         this.setData({
           timeData: e.detail,
         });
     },
     start() {
+        console.log({
+            leftNumberOfReminders: this.data.leftNumberOfReminders,
+        })
+        if (this.data.leftNumberOfReminders === 0) {
+            return
+        }
         const that = this;
         const countDown = this.selectComponent('.control-count-down');
         countDown.start();
@@ -68,32 +81,48 @@ Page({
     reset() {
         const countDown = this.selectComponent('.control-count-down');
         countDown.reset();
-    },
-
-    finished() {
-        Toast('倒计时结束');
-        const that = this;
-        const leftTimes = that.data.leftNumberOfReminders - 1
-        console.log({
-            leftNumberOfReminders: that.data.leftNumberOfReminders,
-            leftTimes,
-            time: this.data.time,
-            newtime: this.data.interval * 60 * 1000,
+        this.setData({
+            recordList: [],
+            leftNumberOfReminders: this.data.numberOfReminders,
         })
+    },
+    recordListAdd() {
+        const recordList = this.data.recordList
+        recordList.push({
+            time: moment().format('YYYY-MM-DD HH:mm:ss'),
+            num: recordList.length + 1,
+        })
+        this.setData({
+            recordList,
+        })
+    },
+    finished() {
+        // Toast('倒计时结束');
+        const that = this;
+        this.recordListAdd()
+        const leftTimes = that.data.leftNumberOfReminders - 1
+        // console.log({
+        //     leftNumberOfReminders: that.data.leftNumberOfReminders,
+        //     leftTimes,
+        //     time: this.data.time,
+        //     newtime: this.data.interval * 60 * 1000,
+        // })
         // this.setData({
         //     leftNumberOfReminders: leftTimes,
         // })
-        if (leftTimes > 0) {
-            this.setData({
+        if (leftTimes >= 0) {
+            that.setData({
                 // time: this.data.interval * 60 * 1000,
                 leftNumberOfReminders: leftTimes,
-            }, () => {
-                // setTimeout(() => {
-                //     that.start()
-                // }, 16);
             })
-            const countDown = this.selectComponent('.control-count-down');
-            countDown.reset();
+        }
+        if (leftTimes > 0) {
+            
+            // setTimeout(() => {
+                const countDown = this.selectComponent('.control-count-down');
+                countDown.reset();
+                countDown.start();
+            // }, 1000);
         }
         
         this.playAudio()
@@ -112,16 +141,24 @@ Page({
 
         const innerAudioContext = wx.createInnerAudioContext();
         innerAudioContext.autoplay = true;
-        innerAudioContext.src = 'https://m801.music.126.net/20231106234303/36c09a91d6f5f106bffae42aa780caaf/jdyyaac/005c/005d/0558/76513378ae773cb1c43a226f40a1bc93.m4a'; // 请替换为你的音频文件路径
+        // innerAudioContext.src = 'https://m701.music.126.net/20231107195255/557d60df41dfb6a27d532c9a5072db9a/jdyyaac/005c/005d/0558/76513378ae773cb1c43a226f40a1bc93.m4a'; // 请替换为你的音频文件路径
+        innerAudioContext.src = 'https://webfs.hw.kugou.com/202311072005/94913d7c0a53381a92b4ef916e281876/v2/690fb99a871dbc14cbfdc43bef16a829/G209/M06/0C/17/EQ4DAF6wq0uAQkBrACTskSjWf6Y926.mp3'
         innerAudioContext.onPlay(() => {
             console.log('开始播放');
+            // Toast('开始播放');
         });
 
         setTimeout(() => {
             innerAudioContext.stop()
-        }, 1000 * 10);
+        }, 1000 * this.data.songPlaySeconds);
         innerAudioContext.onError((res) => {
+            // Toast('播放错误');
             console.log('播放错误', res.errMsg);
         });
-    }
+    },
+    onClickLeft() {
+        wx.navigateTo({
+            url: '/pages/setting/index',
+        })
+    },
 })
--- miniprogram/pages/setting/index.ts
+++ miniprogram/pages/setting/index.ts
@@ -7,6 +7,7 @@ Page({
     data: {
         interval: 0,
         numberOfReminders: 0,
+        songPlaySeconds: 0,
     },
     // 事件处理函数
     bindViewTap() {
@@ -22,6 +23,7 @@ Page({
                 let resObj: {
                     interval?: number
                     numberOfReminders?: number
+                    songPlaySeconds?: number
                 } = {}
                 try {
                     resObj = JSON.parse(res.data)
@@ -31,18 +33,24 @@ Page({
                 that.setData({
                     interval: resObj?.interval || 10,
                     numberOfReminders: resObj?.numberOfReminders || 10,
+                    songPlaySeconds: resObj?.songPlaySeconds || 10,
                 })
             },
             fail(res) {
                 that.setData({
                     interval: 10,
                     numberOfReminders: 3,
+                    songPlaySeconds: 10,
                 })
             }
         })
     },
     onClickLeft() {
-        wx.showToast({ title: '点击返回', icon: 'none' });
+        // wx.showToast({ title: '点击返回', icon: 'none' });
+        wx.navigateBack();
+        // wx.reLaunch({
+        //     url: '/pages/index/index',
+        // })
     },
     handleSave() {
         wx.setStorage({
@@ -50,8 +58,12 @@ Page({
             data: JSON.stringify({
                 interval: +this.data.interval,
                 numberOfReminders: +this.data.numberOfReminders,
+                songPlaySeconds: +this.data.songPlaySeconds,
             })
         })
         Toast.success('保存成功');
+        wx.reLaunch({
+            url: '/pages/index/index',
+        })
     }
 })
